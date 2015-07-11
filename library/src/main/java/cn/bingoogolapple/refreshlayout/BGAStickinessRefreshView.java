e6cd321e26449cbe4657c72c21218025d443ae86
bingoogolapple <bingoogolapple@gmail.com>
2015-07-11 19:57:11 +0800

    优化黏性控件

--- library/src/main/java/cn/bingoogolapple/refreshlayout/BGAStickinessRefreshView.java
+++ library/src/main/java/cn/bingoogolapple/refreshlayout/BGAStickinessRefreshView.java
@@ -106,7 +106,7 @@ public class BGAStickinessRefreshView extends View {
         mRotateDrawableSize = BGARefreshLayout.dp2px(getContext(), 30);
         mTopSize = mRotateDrawableSize + 2 * mEdge;
 
-        mMaxBottomHeight = (int) (2.5f * mRotateDrawableSize);
+        mMaxBottomHeight = (int) (2.4f * mRotateDrawableSize);
     }
 
     @Override
@@ -163,7 +163,7 @@ public class BGAStickinessRefreshView extends View {
             // scale                  0.2 到 1
             float scale = Math.max(mCurrentBottomHeight * 1.0f / mMaxBottomHeight, 0.2f);
 
-            float bottomControlXOffset = mTopSize * ((4 + (float) Math.pow(scale, 7) * 15) / 32);
+            float bottomControlXOffset = mTopSize * ((3 + (float) Math.pow(scale, 7) * 16) / 32);
             float bottomControlY = mTopBound.bottom / 2 + mCenterPoint.y / 2;
             // 三阶贝塞尔曲线，前两个是控制点，最后一个点是终点
             mPath.cubicTo(mTopBound.right - mTopSize / 8, mTopBound.bottom, mTopBound.right - bottomControlXOffset, bottomControlY, mBottomBound.right, mBottomBound.bottom - mBottomBound.height() / 2);

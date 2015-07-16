0ce4a14b744cb5a6378bd3d20b58ec6a8124eca1
王浩 <bingoogolapple@gmail.com>
2015-07-16 21:11:34 +0800

    下班！

--- library/src/main/java/cn/bingoogolapple/refreshlayout/BGARefreshLayout.java
+++ library/src/main/java/cn/bingoogolapple/refreshlayout/BGARefreshLayout.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 bingoogolapple
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -126,6 +126,27 @@ public class BGARefreshLayout extends LinearLayout {
     private boolean mIsShowLoadingMoreView = true;
 
     private Handler mHandler;
+
+    /**
+     * 是否具有下拉刷新功能
+     */
+    private boolean mWhetherHavePullDownRefresh = true;
+    /**
+     * 是否具有上拉加载更多功能
+     */
+    private boolean mWhetherHavePullUpLoadingMore = true;
+    /**
+     * 下拉刷新是否可用
+     */
+    private boolean mPullDownRefreshEnabled = true;
+    /**
+     * 上拉加载更多是否可用
+     */
+    private boolean mPullUpLoadingMoreEnabled = true;
+    /**
+     * 还剩多少条数据未显示时触发加载更多事件
+     */
+    private int mPullUpLoadingMoreThreshold = 5;
 
     public BGARefreshLayout(Context context) {
         this(context, null);
